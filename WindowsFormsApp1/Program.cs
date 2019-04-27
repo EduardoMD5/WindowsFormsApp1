c29d545ff98c4ddb9d2a4cdb4103adf3f05e4196
eduardo monge <EL_LALO92@hotmail.com>
2019-04-27 14:28:31 -0700

    si

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -15,6 +15,7 @@ namespace WindowsFormsApp1
         static void Main()
         {
             //lo que tu quieras
+            //yeah
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
